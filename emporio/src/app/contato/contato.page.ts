c8f1e20747e65807d96b5ab61db5e7df6f09f1c2
thzxd1 <thziin123@gmail.com>
2024-10-06 14:55:33 -0300

    api maps

--- emporio/src/app/contato/contato.page.ts
+++ emporio/src/app/contato/contato.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-contato',
@@ -14,7 +15,30 @@ export class ContatoPage implements OnInit {
   toggleSearch() {
     this.showSearch = !this.showSearch; // Alterna a visibilidade da caixa de pesquisa
   }
+  map!: L.Map;
+
   ngOnInit() {
+    this.loadMap();
   }
 
+  loadMap() {
+    // Inicializa o mapa
+    this.map = L.map('map').setView([-22.918994, -43.633887], 18); // Coordenadas da localização
+  
+    // Adiciona a camada de tiles do Esri
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 18,
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri',
+    }).addTo(this.map);
+  
+    // Forçar o redesenho do mapa
+    this.map.invalidateSize();
+  
+    // Adiciona um marcador na localização
+    L.marker([-22.918994, -43.633887]).addTo(this.map)
+      .bindPopup('Emporio da Roça GH')
+      .openPopup();
+  }
+  
 }
+
