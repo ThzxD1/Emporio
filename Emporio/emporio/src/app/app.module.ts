b9b3ea7e5be38cce2ae31a432cead855c182e23b
thzxd1 <thziin123@gmail.com>
2024-10-15 21:41:29 -0300

    FireBase Login/cadastro

--- Emporio/emporio/src/app/app.module.ts
+++ Emporio/emporio/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(),  AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),  AppRoutingModule, AngularFireModule, 
+    AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- Emporio/emporio/src/app/cadastro/cadastro.page.ts
+++ Emporio/emporio/src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { AutheticationService } from '../authetication.service';
+import { pawSharp } from 'ionicons/icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,13 +13,48 @@ import { Component, OnInit } from '@angular/core';
 export class CadastroPage implements OnInit {
   showSearch: boolean = false; // Variável para controlar a visibilidade da caixa de pesquisa
   searchQuery: string = ''; // Variável para armazenar a consulta de pesquisa
+  regForm!: FormGroup;
 
-  constructor() {}
+  constructor(public formBuilder:FormBuilder, public loadingCtrl: LoadingController, public authService:AutheticationService, public router : Router) {}
 
   toggleSearch() {
     this.showSearch = !this.showSearch; // Alterna a visibilidade da caixa de pesquisa
   }
   ngOnInit() {
+    this.regForm = this.formBuilder.group({
+      fullname :['',[Validators.required]],
+      email :['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password:['',
+        Validators.required
+      ]
+    })
+  }
+  get errorControl(){
+    return this.regForm?.controls;
+  }
+  
+  async signUp(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if(this.regForm?.valid){
+      const user = await this.authService.registerUser(this.regForm.value.email,this.regForm.value.password).catch(()=>{
+
+      }).catch((error)=>{
+        console.log(error);
+        loading.dismiss()
+      })    
+
+      if(user){
+        loading.dismiss()
+        this.router.navigate(['/home'])
+      }else{
+        console.log('Provide correct value')
+
+      }
+    }
   }
 
 }
